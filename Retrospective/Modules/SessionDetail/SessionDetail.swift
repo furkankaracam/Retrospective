fdbdde2a006183af1cd65289f3f789ad368e89b9
Furkan KARAÇAM <furkankaracam2706@gmail.com>
2024-08-06 10:27:09 +0300

    - Fixed issue where blank comments were initially added when creating a column

--- Retrospective/Modules/SessionDetail/SessionDetail.swift
+++ Retrospective/Modules/SessionDetail/SessionDetail.swift
@@ -11,7 +11,7 @@ struct SessionDetail: View {
     @StateObject private var viewModel = SessionDetailViewModel()
     @State private var isEditing: Bool = false
     @State private var newComment: String = ""
-    @State private var showingCommentInput: String? = nil
+    @State private var showingCommentInput: String? = ""
     
     @State var sessionId: String
     @State var timer: Int
@@ -28,9 +28,9 @@ struct SessionDetail: View {
                     Text("\(timer)")
                         .bold()
                 })
-                
             })
             .padding(.horizontal)
+            
             List {
                 ForEach($viewModel.columns, id: \.id, editActions: .move) { $column in
                     
@@ -38,65 +38,65 @@ struct SessionDetail: View {
                         ColumnTitle(title: columnName)
                             .moveDisabled(true)
                     }
+                    
                     if let comments = column.comments {
-                        if comments.count > 1 {
+                        if comments.count > 0 {
                             ForEach(Array(comments.values), id: \.id) { comment in
                                 CommentCard(isEditing: .constant(false), card: Comment(id: comment.id, author: comment.author, comment: comment.comment))
                             }
                         }
-                        if showingCommentInput == comments.keys.first {
-                            TextField("Yeni yorumunuzu yazın", text: $newComment)
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                                .padding()
-                        }
-                        HStack {
-                            if showingCommentInput != comments.keys.first {
-                                Button("Yeni Ekle") {
-                                    showingCommentInput = comments.keys.first
-                                    if newComment != "" {
-                                        newComment = ""
-                                    }
-                                }
-                            } else {
-                                Button("Vazgeç") {
-                                    showingCommentInput = nil
-                                    newComment = ""
-                                }
-                                .tint(.red)
-                            }
-                            
-                            
-                            if !newComment.isEmpty {
-                                Button("Yorum Gönder") {
-                                    Task {
-                                        if !newComment.isEmpty {
-                                            await viewModel.addComment(sessionId: sessionId, to: column.id ?? "", comment: newComment)
-                                            newComment = ""
-                                            showingCommentInput = nil
-                                        }
-                                    }
-                                }
-                                .buttonStyle(.bordered)
-                                .padding()
-                            }
-                        }
-                        .buttonStyle(.bordered)
-                        
-                    }
                     }
                     
-            }
-            .task {
-                if !sessionId.isEmpty {
-                    await viewModel.fetchColumns(id: sessionId)
+                    if showingCommentInput == column.id {
+                        TextField("Yeni yorumunuzu yazın", text: $newComment)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding()
+                    }
+                    
+                    HStack {
+                        if showingCommentInput != column.id {
+                            Button("Yeni Ekle") {
+                                showingCommentInput = column.id
+                                if newComment != "" {
+                                    newComment = ""
+                                }
+                            }
+                        } else {
+                            Button("Vazgeç") {
+                                showingCommentInput = nil
+                                newComment = ""
+                            }
+                            .tint(.red)
+                        }
+                        
+                        if !newComment.isEmpty {
+                            Button("Yorum Gönder") {
+                                Task {
+                                    if !newComment.isEmpty {
+                                        await viewModel.addComment(sessionId: sessionId, to: column.id ?? "", comment: newComment)
+                                        newComment = ""
+                                        showingCommentInput = nil
+                                    }
+                                }
+                            }
+                            .buttonStyle(.bordered)
+                            .padding()
+                        }
+                    }
+                    .buttonStyle(.bordered)
                 }
             }
-            .environment(\.editMode, isEditing ? .constant(.active) : .constant(.inactive))
-            .listStyle(.plain)
         }
+        .task {
+            if !sessionId.isEmpty {
+                await viewModel.fetchColumns(id: sessionId)
+            }
+        }
+        .environment(\.editMode, isEditing ? .constant(.active) : .constant(.inactive))
+        .listStyle(.plain)
     }
 }
 
 #Preview {
-    SessionDetail( sessionId: "-O3XEnBJtrBjIc4O1m-x", timer: 60, sessionName: "Sezon")
+    SessionDetail(sessionId: "-O3XEnBJtrBjIc4O1m-x", timer: 60, sessionName: "Sezon")
 }
